38e9df9ecf2e3659f46e4ac076b96f61042e864f
AbdulMoazzim <you@example.com>
2024-03-31 00:47:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 
 
 const showTime = () => {
-    let Days = ['Monday', ' Tuesday', 'Wednesday', 'Thursday', ' Friday', 'Saturday', 'Sunday']
+    let Days = ['Sunday', 'Monday', ' Tuesday', 'Wednesday', 'Thursday', ' Friday', 'Saturday']
     let time = new Date()
     let hour = time.getHours()
     let min = time.getMinutes()
     let sec = time.getSeconds()
     let date = time.getDate()
     let year = time.getFullYear()
-    let day = time.getDay()
+    let DAY = time.getDay()
     let month = time.getMonth()
     document.querySelector('#date').innerHTML = `${date}:${month+1}:${year}`
-    document.querySelector('#day').innerHTML = Days[day-1]
+    document.querySelector('#day').innerHTML = Days[DAY]
     if (hour >= 12){
         document.querySelector('#Time').innerHTML = `${hour-12}:${min}:${sec}`
         document.querySelector('#PMAM').innerHTML = 'PM'
 
     } else {
-        document.querySelector('#Time').innerHTML = `${hour-12}:${min}:${sec}`
+        document.querySelector('#Time').innerHTML = `${hour}:${min}:${sec}`
         document.querySelector('#PMAM').innerHTML = 'AM'
     }
 
